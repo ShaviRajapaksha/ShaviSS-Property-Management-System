0a6dd3dd78b277a858abf597eba9f1792a008839
IT20272654 <shavindurajapaksha@gmail.com>
2026-03-26 02:18:13 +0530

    feat: add HotelsModule to AppModule imports

--- Backend/src/app.module.ts
+++ Backend/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
+import { HotelsModule } from './hotels/hotels.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, HotelsModule],
   controllers: [AppController],
   providers: [AppService],
 })
