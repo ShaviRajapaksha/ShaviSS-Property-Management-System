719980f69052cc47129df8c95cd1452378a33362
IT20272654 <shavindurajapaksha@gmail.com>
2026-03-26 22:43:48 +0530

    feat: add CreateRoomDto for room creation with validation

---
+++ Backend/src/rooms/dto/create-room.dto.ts
@@ -0,0 +1,39 @@
+import { ApiProperty } from "@nestjs/swagger";
+import { RoomType } from "@prisma/client";
+import { IsEnum, IsInt, IsNumber, IsOptional, IsString, Min } from "class-validator";
+
+export class CreateRoomDto {
+    @ApiProperty()
+    @IsString()
+    roomNumber!: string;
+
+    @ApiProperty({ enum: ['SINGLE', 'DOUBLE', 'TWIN', 'SUITE', 'DELUXE', 'PRESIDENTIAL'] })
+//  @ApiProperty({ enum: RoomType })    
+    @IsEnum(RoomType)
+    type!: RoomType;
+
+    @ApiProperty()
+    @IsInt()
+    @Min(1)
+    floor!: number;
+
+    @ApiProperty()
+    @IsInt()
+    @Min(1)
+    capacity!: number;    
+
+    @ApiProperty()
+    @IsNumber()
+    @Min(0)
+    PricePerNight!: number;
+
+    @ApiProperty({required:false})
+    @IsOptional()
+    @IsString()
+    description?: string;
+
+    @ApiProperty()
+    @IsString()
+    hotelId!: string;
+}
+
