854b192c9720373b6d9b714c896ff41a3cf85076
IT20272654 <shavindurajapaksha@gmail.com>
2026-03-27 22:34:19 +0530

    feat: add GuestsModule to app imports for guest management integration

--- Backend/src/app.module.ts
+++ Backend/src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { HotelsModule } from './hotels/hotels.module';
 import { RoomsModule } from './rooms/rooms.module';
+import { GuestsModule } from './guest/guests.module';
 
 @Module({
-  imports: [PrismaModule, HotelsModule, RoomsModule],
+  imports: [PrismaModule, HotelsModule, RoomsModule, GuestsModule],
   controllers: [AppController],
   providers: [AppService],
 })
